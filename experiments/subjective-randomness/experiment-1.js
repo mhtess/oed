69c6e4d81751d7887a6e56b7fdc091f0111f1765
mhtess <mtessler@stanford.edu>
2016-05-07 16:14:40 -0700

    display sequence sequentially

--- experiments/subjective-randomness/experiment-1.js
+++ experiments/subjective-randomness/experiment-1.js
@@ -31,6 +31,7 @@ function make_slides(f) {
       exp.go(); //use exp.go() if and only if there is no "present" data.
     }
   });
+
   slides.check = slide({
      name : "check",
      start: function() {
@@ -57,32 +58,48 @@ function make_slides(f) {
       });
     }
   });
+
   slides.coins = slide({
     name: "coins",
     present : exp.stims,
     //this gets run only at the beginning of the block
     present_handle : function(stim) {
 
-       var keyDictionary = {"fair-key": "ordinary, fair coin",
-            "unfair-key": "weighted, trick coin"}
+      this.displaySequence = []
+      this.sequence = stim.split(' ').slice(0,4)
 
 
+      $(".prompt").html("Press the space bar to flip the coin.")
       this.startTime = Date.now();
       this.stim = stim 
-      $(".evidence").html(this.stim);
+      // $(".evidence").html(this.stim);
 
-      _.mapObject(exp.coinButtons, function(val,key){
-        $("#"+val+"key-reminder").html(keyDictionary[key]);
-      });
+      $("#reminders").hide()
 
       $(document).one("keydown", _s.keyPressHandler);
     },
 
     keyPressHandler : function(event) {
       var keyCode = event.which;
-      if (keyCode != 81 && keyCode != 80) {
+
+      if (keyCode != 81 && keyCode != 80 && keyCode != 32) {
         // If a key that we don't care about is pressed, re-attach the handler (see the end of this script for more info)
         $(document).one("keydown", _s.keyPressHandler);
+
+      } else if (keyCode == 32 && _s.sequence.length!=0) {
+
+        _s.displaySequence.push(_s.sequence.pop())
+        $(".evidence").html(_s.displaySequence);
+        $(document).one("keydown", _s.keyPressHandler);
+
+        if (_s.sequence.length==0) {
+          $("#reminders").show()
+          _.mapObject(exp.coinButtons, function(val,key){
+            $("#"+val+"key-reminder").html(exp.keyDictionary[key]);
+          });
+          $(".prompt").html("Predict the next outcome.")
+        }
+
       } else {
         // If a valid key is pressed (code 80 is p, 81 is q),
           _s.rt = Date.now() - _s.startTime;
@@ -103,311 +120,6 @@ function make_slides(f) {
         "rt":_s.rt,
       });
     }
-  });
-
-
-  slides.implied_prevalence = slide({
-    name: "implied_prevalence",
-
-   // present : _.shuffle(_.range(numTrials)),
-  present : _.range(exp.numTrials),
-    //this gets run only at the beginning of the block
-    present_handle : function(stim_num) {
-      this.startTime = Date.now();
-
-      $(".err").hide();
-      $("#text_response").val('')
-
-      this.stim = exp.stims[stim_num]; // allstims should be randomized, or stim_num should be
-      this.trialNum = stim_num;
-
-      //  the following commands work only because there are "3 lists" of stimuli, and there are 3 exp.stimtypes (also 3 exp.deteminers)
-      //this.determiner = exp.determiner[this.stim.list] // exp.determiner already randomized, grab which stimtype corresponds to list #_this.stim
-      this.determiner = exp.determiner[0] // exp.determiner between-subjects var
-      this.stimtype = exp.stimtype[this.stim.list]; // exp.stimtype already randomized, grab which stimtype corresponds to list #_this.stim
-      //this.stimtype = exp.stimtype[0]; // exp.stimtype between-subjects var
-
-      var query_prompt = "What percentage of "  + this.stim.category + " do you think have " + this.stim.color + " " + this.stim.part + "?\n";
-
-
-      this.stimtype == 'danger' ? this.adjective = 'dangerous ' : null;
-      this.stimtype == 'distinct' ? this.adjective = 'distinctive ': null;
-      this.stimtype == 'irrelevant' ? this.adjective = this.stim.extraneous + ' ': null;
-
-      this.determiner=='generic' ?
-        this.evidence_prompt = utils.upperCaseFirst(this.stim.category) + " have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n" :
-        this.evidence_prompt = utils.upperCaseFirst(this.determiner) + " " + this.stim.category +" have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n";
-
-      this.stimtype=='danger' ? this.evidence_prompt+=this.stim.dangerous:null;
-      this.stimtype=='distinct' ? this.evidence_prompt+=this.stim.distinctive:null;
-      this.stimtype=='irrelevant' ? this.evidence_prompt+=this.stim.irrelevant:null;
-
-
-      $(".evidence").html(this.evidence_prompt);
-      $(".query").html(query_prompt);
-
-       // this.init_radiios();
-       // exp.sliderPost = null; //erase current slider value
-    },
-
-    button : function() {
-      response = $("#text_response").val();
-      if (!(response<=100 && response>=0 && response!='')) {
-        $(".err").show();
-      } else {
-        this.rt = Date.now() - this.startTime;
-        this.log_responses();
-
-        /* use _stream.apply(this); if and only if there is
-        "present" data. (and only *after* responses are logged) */
-        _stream.apply(this);
-
-
-      //   exp.data_trials.push({
-      //     "trial_type" : "single_trial",
-      //     "response" : response
-      //   });
-      //   exp.go(); //make sure this is at the *end*, after you log your data
-      // }
-      }
-
-    },
-
-        log_responses : function() {
-      exp.data_trials.push({
-        "trial_type" : "implied_prevalence",
-        "trial_num": this.trialNum,
-        "response" : $("#text_response").val(),
-        "rt":this.rt,
-        "stim_type": this.stimtype,
-        "stim_determiner": this.determiner,
-        "stim_category": this.stim.category,
-        "stim_color": this.stim.color,
-        "stim_part":this.stim.part
-      });
-    }
-  });
-
-  slides.truth_conditions = slide({
-    name: "truth_conditions",
-
-
-    /* trial information for this block
-     (the variable 'stim' will change between each of these values,
-      and for each of these, present_handle will be run.) */
-    present : _.range(exp.numTrials),
-
-    //this gets run only at the beginning of the block
-    present_handle : function(stim_num) {
-      this.startTime = Date.now();
-      $(".err").hide();
-      $('input[name="radio_button"]').prop('checked', false);
-
-      this.stim = exp.stims[stim_num]; // allstims should be randomized, or stim_num should be
-      this.trialNum = stim_num;
-
-      //  the following commands work only because there are "3 lists" of stimuli, and there are 3 exp.stimtypes (also 3 exp.deteminers)
-      //this.determiner = exp.determiner[this.stim.list] // exp.determiner already randomized, grab which stimtype corresponds to list #_this.stim
-      this.determiner = exp.determiner[0] // exp.determiner between-subjects var
-      // BAD
-      //this.stimtype = exp.stimtype[this.stim.list]; // exp.stimtype already randomized, grab which stimtype corresponds to list #_this.stim
-      // BETTER
-      this.stimtype = exp.stims[stim_num].context
-
-      //this.stimtype = exp.stimtype[0]; // exp.stimtype between-subjects var
-      this.prevalence = exp.prevalence_levels[this.stim.list].splice(0,1)[0] // grab prevalence level for this list
-
-      this.stimtype == 'bare' ? this.adjective = '' : null;      
-      this.stimtype == 'danger' ? this.adjective = 'dangerous ' : null;
-      this.stimtype == 'distinct' ? this.adjective = 'distinctive ': null;
-      this.stimtype == 'irrelevant' ? this.adjective = this.stim.extraneous + ' ': null;
-      this.stimtype == 'danger-distinct' ? this.adjective = 'dangerous ' : null;
-      this.stimtype == 'nondistinctive' ? this.adjective = this.stim.extraneous + ' ': null;
-
-      this.evidence_prompt = this.prevalence+ "% of "  + this.stim.category + " have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n";
-
-      // if (this.determiner=='generic'){
-      //   var query_prompt = utils.upperCaseFirst(this.stim.category) + " have " + this.stim.color + " " + this.stim.part + ".\n";
-      // }
-      // else{
-      //   var query_prompt = utils.upperCaseFirst(this.determiner) + " " + this.stim.category +" have " + this.stim.color + " " + this.stim.part + ".\n";
-      // }
-
-      // if (this.stimtype == 'danger'){
-      //   evidence_prompt+=this.stim.danger +"\n No other animals on this island have this kind of " + this.stim.part
-      // }
-
-      // if (this.stimtype == 'irrelevant'){
-      //   evidence_prompt+=this.stim.irrelevant +"\n Other animals on this island also have this kind of " + this.stim.part
-      // }
-
-      // this.determiner=='generic' ?
-      //   (this.stimtype=='bare' ?
-      //     this.query_prompt = utils.upperCaseFirst(this.stim.category) + " have " + this.stim.color + " " + this.stim.part + ".\n" :
-      //     this.query_prompt = utils.upperCaseFirst(this.stim.category) + " have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n"
-      //     ) :
-      //   (this.stimtype=='bare' ?
-      //     this.query_prompt = utils.upperCaseFirst(this.determiner) + " " + this.stim.category +" have " + this.stim.color + " " + this.stim.part + ".\n":
-      //     this.query_prompt = utils.upperCaseFirst(this.determiner) + " " + this.stim.category +" have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n"
-      //   );
-
-      this.determiner=='generic' ?
-        this.query_prompt = utils.upperCaseFirst(this.stim.category) + " have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n":
-        this.query_prompt = utils.upperCaseFirst(this.determiner) + " " + this.stim.category +" have " + this.adjective + this.stim.color + " " + this.stim.part + ".\n";
-
-      this.stimtype=='danger' ? this.evidence_prompt+=this.stim.dangerous:null;
-      this.stimtype=='distinct' ? this.evidence_prompt+=this.stim.distinctive:null;
-      this.stimtype=='irrelevant' ? this.evidence_prompt+=this.stim.irrelevant:null;
-      this.stimtype == 'danger-distinct' ? this.evidence_prompt+=(this.stim.dangerous + ' ' + this.stim.dangdistinctive):null;
-      this.stimtype=='nondistinctive' ? this.evidence_prompt+=(this.stim.irrelevant + ' ' + this.stim.nondistinctive):null;
-
-      $(".evidence").html(this.evidence_prompt);
-      $(".query").html(this.query_prompt);
-
-       // this.init_radiios();
-       // exp.sliderPost = null; //erase current slider value
-    },
-
-    button : function() {
-      if (!($("input:radio[name=radio_button]").is(":checked"))) {
-        $(".err").show();
-      } else {
-        this.rt = Date.now() - this.startTime;
-        this.log_responses();
-
-        /* use _stream.apply(this); if and only if there is
-        "present" data. (and only *after* responses are logged) */
-        _stream.apply(this);
-      }
-    },
-
-    log_responses : function() {
-      exp.data_trials.push({
-        "trial_type" : "truth_conditions",
-        "trialNum":this.trialNum,
-        "response" : $("input:radio[name=radio_button]:checked").val(),
-        "rt":this.rt,
-        "stim_type": this.stimtype,
-        "stim_prevalence": this.prevalence,
-        "stim_determiner": this.determiner,
-        "stim_category": this.stim.category,
-        "stim_color": this.stim.color,
-        "stim_part":this.stim.part
-      });
-    }
-  });
-
-  slides.one_slider = slide({
-    name : "one_slider",
-
-    /* trial information for this block
-     (the variable 'stim' will change between each of these values,
-      and for each of these, present_handle will be run.) */
-    present : [
-      {subject: "dog", object: "ball"},
-      {subject: "cat", object: "windowsill"},
-      {subject: "bird", object: "shiny object"},
-    ],
-
-    //this gets run only at the beginning of the block
-    present_handle : function(stim) {
-      $(".err").hide();
-
-      this.stim = stim; //I like to store this information in the slide so I can record it later.
-
-      $(".prompt").html(stim.subject + "s like " + stim.object + "s.");
-      this.init_sliders();
-      exp.sliderPost = null; //erase current slider value
-    },
-
-    button : function() {
-      if (exp.sliderPost == null) {
-        $(".err").show();
-      } else {
-        this.log_responses();
-
-        /* use _stream.apply(this); if and only if there is
-        "present" data. (and only *after* responses are logged) */
-        _stream.apply(this);
-      }
-    },
-
-    init_sliders : function() {
-      utils.make_slider("#single_slider", function(event, ui) {
-        exp.sliderPost = ui.value;
-      });
-    },
-
-    log_responses : function() {
-      exp.data_trials.push({
-        "trial_type" : "one_slider",
-        "response" : exp.sliderPost
-      });
-    }
-  });
-
-  slides.multi_slider = slide({
-    name : "multi_slider",
-    present : _.shuffle([
-      {"critter":"Wugs", "property":"fur"},
-      {"critter":"Blicks", "property":"fur"}
-    ]),
-    present_handle : function(stim) {
-      $(".err").hide();
-      this.stim = stim; //FRED: allows you to access stim in helpers
-
-      this.sentence_types = _.shuffle(["generic", "negation", "always", "sometimes", "usually"]);
-      var sentences = {
-        "generic": stim.critter + " have " + stim.property + ".",
-        "negation": stim.critter + " do not have " + stim.property + ".",
-        "always": stim.critter + " always have " + stim.property + ".",
-        "sometimes": stim.critter + " sometimes have " + stim.property + ".",
-        "usually": stim.critter + " usually have " + stim.property + "."
-      };
-
-      this.n_sliders = this.sentence_types.length;
-      $(".slider_row").remove();
-      for (var i=0; i<this.n_sliders; i++) {
-        var sentence_type = this.sentence_types[i];
-        var sentence = sentences[sentence_type];
-        $("#multi_slider_table").append('<tr class="slider_row"><td class="slider_target" id="sentence' + i + '">' + sentence + '</td><td colspan="2"><div id="slider' + i + '" class="slider">-------[ ]--------</div></td></tr>');
-        utils.match_row_height("#multi_slider_table", ".slider_target");
-      }
-
-      this.init_sliders(this.sentence_types);
-      exp.sliderPost = [];
-    },
-
-    button : function() {
-      if (exp.sliderPost.length < this.n_sliders) {
-        $(".err").show();
-      } else {
-        this.log_responses();
-        _stream.apply(this); //use _stream.apply(this); if and only if there is "present" data.
-      }
-    },
-
-    init_sliders : function(sentence_types) {
-      for (var i=0; i<sentence_types.length; i++) {
-        var sentence_type = sentence_types[i];
-        utils.make_slider("#slider" + i, this.make_slider_callback(i));
-      }
-    },
-    make_slider_callback : function(i) {
-      return function(event, ui) {
-        exp.sliderPost[i] = ui.value;
-      };
-    },
-    log_responses : function() {
-      for (var i=0; i<this.sentence_types.length; i++) {
-        var sentence_type = this.sentence_types[i];
-        exp.data_trials.push({
-          "trial_type" : "multi_slider",
-          "sentence_type" : sentence_type,
-          "response" : exp.sliderPost[i]
-        });
-      }
-    },
   });
 
   slides.subj_info =  slide({
@@ -450,29 +162,28 @@ function make_slides(f) {
 /// init ///
 function init() {
   var prev_levels = ["10","10","30","30","50","50","70","70","90","90"];
-//  var contexts = ["bare","danger-distinct","nondistinctive"];
   var contexts = ["bare","danger","distinct"];
 
-    var coin = ["H ","T "]
-  exp.condition = '5coins';
+  var coin = ["H ","T "]
+  exp.condition = '4_coins';
   exp.buttonCodes = {80:"P", 81:"Q"};
-  exp.coinButtons = _.object(_.zip(["fair-key","unfair-key"],
+  exp.coinButtons = _.object(_.zip(["heads-key","tails-key"],
                             _.shuffle(["P","Q"])));
 
 
+  exp.keyDictionary = {
+    "heads-key": "H",
+    "tails-key": "T"
+  }
 
   exp.trials = [];
   exp.catch_trials = [];
-//  exp.condition = _.sample(["truth_conditions", "implied_prevalence"]); //can randomize between subject conditions here
-  exp.condition = "trick-fair";
-//  exp.stimtype = _.shuffle(["bare","danger","irrelevant"]);
-//  exp.stimtype = ["bare","danger/distinct","nondistinctive"]; //because there is list1, list2, list3
-//  exp.determiner = _.shuffle(["generic","some","most"]);
+  exp.condition = "predictive";
   exp.determiner = ["generic","generic","generic"];
   exp.instructions = "elaborate_instructions";
 
 
-  exp.stims = _.shuffle(makeCoinSeq(coin,4));
+  exp.stims = _.shuffle(makeCoinSeq(coin,3));
 
   exp.prevalence_levels = [_.shuffle(prev_levels),_.shuffle(prev_levels),_.shuffle(prev_levels)];
 
@@ -485,11 +196,8 @@ function init() {
       screenUW: exp.width
     };
   //blocks of the experiment:
- // exp.structure=["i0", "two_afc","single_trial","two_afc","single_trial", "one_slider", "multi_slider", 'subj_info', 'thanks'];
-   exp.structure=["i0", "coins","check",'subj_info', 'thanks'];
-//   exp.structure=["subj_info"]
-   //exp.structure=['subj_info', 'thanks'];
- 
+   exp.structure=["coins","i0", "coins","check",'subj_info', 'thanks'];
+
   exp.data_trials = [];
   //make corresponding slides:
   exp.slides = make_slides(exp);
